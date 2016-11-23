43b0f46229aad2a3c7a6c504e42653104ded1174
johnhu <huzichunjohn@126.com>
2016-11-23 19:17:18 +0800

    test redux.

--- client/index.jsx
+++ client/index.jsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-import App from './app';
-import About from './about';
-import Repos from './repos';
-import Repo from './repo';
-import Home from './home';
+import SearchBar from './searchBar';
+import GifList from './gifList';
+import request from 'superagent';
 
-ReactDOM.render((
-    <Router history={browserHistory}>
-	<Route path="/" component={App}>
-	    <IndexRoute component={Home} />
-	    <Route path='/repos' component={Repos} >
-	        <Route path='/repos/:userName/:repoName' component={Repo} />
-	    </Route>
-	    <Route path="/about" component={About} />
-	</Route>
-    </Router>
-), document.getElementById('app'));
+class App extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    gifs: [
+		/*
+		{
+		    id: 1,
+		    url: 'http://fakeimg.pl/300/'
+		},
+		{
+		    id: 2,
+		    url: 'http://fakeimg.pl/300/'
+		},
+		{
+		    id: 3,
+		    url: 'http://fakeimg.pl/300/'
+		}
+		*/
+	    ]
+	};
+	this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+
+
+    handleTermChange(term) {
+	const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+	
+	request.get(url, function(err, res) {
+	    this.setState({ gifs: res.body.data });
+	});
+    }
+
+    render() {
+	return (
+	    <div>
+	        <SearchBar onTermChange={this.handleTermChange} />
+		<GifList gifs={this.state.gifs} />
+	    </div>
+	);
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
