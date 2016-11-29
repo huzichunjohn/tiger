c7ca19025a990e7eecc555272177604d7488a343
johnhu <huzichunjohn@126.com>
2016-11-29 17:54:09 +0800

    integrate with firebase.

--- client/components/gifList.jsx
+++ client/components/gifList.jsx
@@ -4,11 +4,17 @@ import GifItem from './gifItem';
 
 const GifList = (props) => {
     const gifItems = props.gifs.map((image) => {
-	return <GifItem key={image.id} gif={image} onGifSelect={props.onGifSelect} />
+	return <GifItem key={image.id}
+			gif={image}
+			onGifSelect={props.onGifSelect}
+			onFavoriteSelect={props.onFavoriteSelect}
+			onFavoriteDeselect={props.onFavoriteDeselect}
+			isAuthenticated={props.isAuthenticated}
+			isFavorite={props.isFavorite} />
     });
 
     return (
-	<ul className="gif-list">{gifItems}</ul>
+	<div className="gif-list">{gifItems}</div>
     );
 };
 
